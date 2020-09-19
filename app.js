3ceafaad1c1265be6ea6d0dcb827bdfb0c97eed9
eyalstolero7 <eyalstolero@campus.technion.ac.il>
2020-09-20 00:27:02 +0300

    added passport authentication with cookies

--- app.js
+++ app.js
@@ -5,37 +5,61 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const e = require("express");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
     res.render("home");
 });
 
 app.get("/logout", function (req, res) {
+    req.logout();
     res.redirect("/");
 });
 
 app.get("/secrets", function (req, res) {
-    res.render("secrets");
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
 });
 
 app.route("/login")
@@ -45,34 +69,21 @@ app.route("/login")
         });
     })
     .post(function (req, res) {
-        const username = req.body.username;
-        const password = req.body.password;
-
-        User.findOne({ email: username }, function (err, foundUser) {
-            if (err) {
-                console.log(err);
-            } else if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function (
-                    err,
-                    result
-                ) {
-                    if (result) {
-                        res.redirect("/secrets");
-                    }
-                    else {
-                        res.render("login", {
-                            wrongLogin:
-                                "* Email and password doesn't match!",
-                        });
-                    }
-                });
-            } else {
-                res.render("login", {
-                    wrongLogin:
-                        "* Email doesn't exists!",
-                });
-            }
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
         });
+
+        req.login(user, function(err) {
+            if(err) {
+                console.log(err);
+            }
+            else {
+                passport.authenticate("local")(req, res, function() {
+                    res.redirect("/secrets");
+                })
+            }
+        })
     });
 
 app.route("/register")
@@ -80,19 +91,19 @@ app.route("/register")
         res.render("register");
     })
     .post(function (req, res) {
-        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-            const user = new User({
-                email: req.body.username,
-                password: hash,
-            });
-            user.save(function (err) {
+        User.register(
+            { username: req.body.username },
+            req.body.password,
+            function (err, user) {
                 if (err) {
                     console.log(err);
                 } else {
-                    res.redirect("/secrets");
+                    passport.authenticate("local")(req, res, function () {
+                        res.redirect("/secrets");
+                    });
                 }
-            });
-        });
+            }
+        );
     });
 
 app.listen(process.env.PORT || 3000, function () {
